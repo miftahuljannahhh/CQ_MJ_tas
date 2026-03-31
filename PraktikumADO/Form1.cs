78afc66da9ffb9e64212a3abcc4c14ceec0f07ee
miftahuljannahhh <mj5416386@gmail.com>
2026-03-31 15:59:25 +0700

    count mahasiswa

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -41,14 +41,32 @@ namespace PraktikumADO
             }
         }
 
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         private void btnUpdate_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void btnHitungMhs_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
