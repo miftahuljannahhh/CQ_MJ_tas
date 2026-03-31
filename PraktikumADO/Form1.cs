9f3f0e96124c9317a2784a2665cc036d9fd75e6b
miftahuljannahhh <mj5416386@gmail.com>
2026-03-31 17:07:46 +0700

    update matakuliah

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -125,7 +125,28 @@ namespace PraktikumADO
             }
         }
 
-        
+        private void btnUpdateMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
 
     }
 }
