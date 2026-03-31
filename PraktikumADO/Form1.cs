8ffc29a7bc9c171fcc6dd30da9c00c42b869e73f
miftahuljannahhh <mj5416386@gmail.com>
2026-03-31 15:57:18 +0700

    Add connect feature

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -26,6 +26,21 @@ namespace PraktikumADO
             );
         }
 
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         private void btnUpdate_Click(object sender, EventArgs e)
         {
 
